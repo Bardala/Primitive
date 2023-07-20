31499befface687e621851a082c3ff6bc684b87f
Bardala <102490981+Bardala@users.noreply.github.com>
2023-07-20 12:08:44 +0300

    using react-query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
-import "./App.css";
+import { Blog, BlogRes } from "@nest/shared";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-  return <div>Hello</div>;
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["getBlog", 1],
+    queryFn: (): Promise<BlogRes> =>
+      fetch("http://localhost:4001/api/v0/blog/1").then((res) => res.json()),
+  });
+
+  const blog: Blog | undefined = data?.blog;
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Something went wrong</div>;
+
+  return (
+    <>
+      <div>Hello</div>
+      {blog && <div>{blog?.content}</div>}
+    </>
+  );
 }
 
 export default App;
