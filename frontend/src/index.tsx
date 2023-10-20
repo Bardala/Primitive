627cc88a94f4d54a1ce1948697267a185cc161e1
Bardala <102490981+Bardala@users.noreply.github.com>
2023-10-20 22:57:28 +0300

    fix some problems

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-// import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
@@ -12,12 +12,12 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const queryClient = new QueryClient();
 
 root.render(
-  // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <AuthContextProvider>
-      <App />
-      <ReactQueryDevtools />
-    </AuthContextProvider>
-  </QueryClientProvider>
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <App />
+        <ReactQueryDevtools />
+      </AuthContextProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
