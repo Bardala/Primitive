36dafd4f18bcd770ff1dcf048e62544bd47d15a9
bardala <islambardala@gmail.com>
2026-02-26 04:03:20 +0200

    refactor: update space ID extraction in ShortForm to use `matchPath` with `ROUTES` instead of `useParams`.

--- frontend/src/features/short-form/components/ShortForm.tsx
+++ frontend/src/features/short-form/components/ShortForm.tsx
@@ -1,14 +1,16 @@
 import { useSideBar } from '@/core/context/SideBarContext';
+import { ROUTES } from '@/core/utils';
 import { BlogForm } from '@/features/blog';
 
 import { DefaultSpaceId } from '@nest/shared';
 
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { matchPath } from 'react-router-dom';
 
 export const ShortForm = () => {
   const { dispatch } = useSideBar();
-  const id = useParams().id || DefaultSpaceId;
-
+  const match = matchPath(ROUTES.SPACE, document.location.pathname);
+  const id = match?.params.id || DefaultSpaceId;
+ 
   return <BlogForm spaceId={id} onSuccess={() => dispatch({ type: 'closeAll' })} />;
 };
